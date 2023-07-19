5b1870d072cf4f2769ea64bee7ffc5b8a47ef81f
angelo ayranji <angeloayranji@gmail.com>
2023-07-19 15:15:39 +0300

    Added soulboundtoken + rewardToken

--- contracts/SBT.sol
+++ contracts/SBT.sol
@@ -5,10 +5,25 @@ import "@openzeppelin/contracts@4.9.2/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.9.2/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@4.9.2/access/Ownable.sol";
 
-contract TruthToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("TruthToken", "Truth") {}
+contract SBT is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("SoulBoundToken", "SBT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function batchMint(address[] memory tos, uint256[] memory amounts) public {
+        require(tos.length == amounts.length, "Invalid input lengths");
+        for (uint256 i = 0; i < tos.length; i++) {
+            _mint(tos[i], amounts[i]);
+        }
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20)
+    {   
+        require(msg.sender == to, "Token transfer is restricted");
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
